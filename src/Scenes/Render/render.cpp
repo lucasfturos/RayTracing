ff0700ecb560d384d38b0e3f95877fd9343fd745
lucasfturos <lucasfturos@gmail.com>
2024-04-03 22:27:07 -0300

    Add CMakeLists.txt src

--- src/Scenes/Render/render.cpp
+++ src/Scenes/Render/render.cpp
@@ -44,13 +44,13 @@ void Render::run() {
     // Camera
     // point3 lookfrom(26, 3, 6);
     // point3 lookfrom(13, 2, 3);
-    point3 lookfrom(0, -3, 5); // visão de frente
+    point3 lookfrom(0, 0, 4.5); // visão de frente
     // point3 lookfrom(3, 3, 2); // Visão da diagonal
     // point3 lookfrom(15, 0, 30); // Posição da câmera para visualização
     // diagonal
 
     //  Visão do observador
-    point3 lookat(0, 0, 0);
+    point3 lookat(0, 2, 0);
     vec3 vup(0, 1, 0);
     color background(0, 0, 0);
     background = color(0.7, 0.7, 0.7);
